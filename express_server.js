e37b9753c726a43a718d8c2b7696238cd8652f45
Kenia Balestra <kenia.balestra@gmail.com>
2022-09-06 23:44:49 +0000

    Add partials

--- express_server.js
+++ express_server.js
@@ -23,7 +23,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
 
   res.render('urls_show', templateVars);
 });
