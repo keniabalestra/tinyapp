2189a6d5bb033cf07bc613f72b52a94d4427aa3e
Kenia Balestra <kenia.balestra@gmail.com>
2022-09-07 00:25:31 +0000

    Add post request

--- express_server.js
+++ express_server.js
@@ -8,6 +8,12 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const generateRandomString = function (){
+  
+}
+
+app.use(express.urlencoded({ extended: true }));//body-parser
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -18,16 +24,20 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-
   res.render('urls_index', templateVars);
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-
   res.render('urls_show', templateVars);
 });
-
+app.post("/urls", (req, res)=> {
+  console.log(req.body);
+  res.send("Ok");
+});
 
 
 app.listen(PORT, () => {
