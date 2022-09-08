69f31632a2f39b6c8d8c268d39140f6e74a61bad
Kenia Balestra <kenia.balestra@gmail.com>
2022-09-08 19:32:09 +0000

    Refactor login route, add login error messages

--- express_server.js
+++ express_server.js
@@ -3,14 +3,18 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
+//middleware
 app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));//body-parser
 
+
+//user databaseish
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 const users = {
   userRandomID: {
     id: "userRandomID",
@@ -18,6 +22,9 @@ const users = {
     password: "purple-monkey-dinosaur",
   }
 };
+
+//GET routes -organization
+//POST routes -organization
 
 
 // program to generate random strings
@@ -42,7 +49,7 @@ app.get("/urls.json", (req, res) => {
 
 
 app.get("/urls", (req, res) => {
-  const userId = req.cookies["user_id"] // userId = abc123
+  const userId = req.cookies["user_id"]; // userId = abc123
   //const user = abc123:{ userId: abc123, email:abc@gmail.com, passwrod: hshdo}
   const templateVars = { urls: urlDatabase, user: users[req.cookies["user_id"]] };
   res.render('urls_index', templateVars);
@@ -74,7 +81,6 @@ app.get("/u/:shortURL", (req, res) => {
 //update
 app.post("/urls/:shortURL", (req, res) => {
   const longURL = req.body.longURL;
-  console.log(longURL);
   urlDatabase[req.params.shortURL] = req.body.longURL;
   res.redirect("/urls");
 });
@@ -90,16 +96,31 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 //Login
 app.post('/login', function(req, res) {
-  const userId = req.cookies["user_id"]
-  res.cookie("user_id",userId);
+  const user = findUserinDatabase(req.body.email);
+console.log(user)
+  //Email not found
+  if (user === null) {
+    return res.status(403).send("Invalid request. Email not found.");
+  }
+
+  // Password doesn't match
+  if (user.password !== req.body.password) {
+    return res.status(403).send("Invalid request. Wrong password.");
+  }
+
+  res.cookie("user_id", user.id);
   res.redirect("/urls");
+
+
 });
 
 //New login
-app.get('/login', (req,res) => {
-  const templateVars = { user: users[req.cookies["user_id"]]};
+app.get('/login', (req, res) => {
+  const templateVars = { user: users[req.cookies["user_id"]] };
   res.render('urls_login', templateVars);
-})
+
+
+});
 
 //Logout
 app.post('/logout', function(req, res) {
@@ -109,22 +130,25 @@ app.post('/logout', function(req, res) {
 
 //Register
 app.get('/register', function(req, res) {
-  const templateVars = { user: users[req.cookies["user_id"]]};
+  const templateVars = { user: users[req.cookies["user_id"]] };
+
   res.render('urls_register', templateVars);
 });
 
 //Register - POST - handles the registration form data
 app.post('/register', function(req, res) {
-  const userId = generateRandomString();
-  users[userId] = { userId, email: req.body.email, password: req.body.password };
-if(!email  || !password){
-  res.status(400).send("Invalid request,. Please add your information. ")
-}
-if (!checkEmail(email, usersDataBase)){
-  res.status(400).send("Invalid request,. Please add your information. ")
-}
-  res.cookie("user_id",userId); //userId= abc123
-  res.redirect('/urls')
+  const newUserId = generateRandomString();
+  users[newUserId] = { id: newUserId, email: req.body.email, password: req.body.password };
+  const user = users[newUserId];
+  if (!user.email || !user.password) {
+    res.status(400).send("Invalid request. Please add your information. ");
+  }
+  if (checkEmail(user.email)) {
+    res.status(400).send("Invalid request. You're already registered.");
+  }
+
+  res.cookie("user_id", newUserId);
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
@@ -132,11 +156,34 @@ app.listen(PORT, () => {
 });
 
 //check to see if email exist
-const checkEmail = (email, usersDataBase) => {
-  for (let key in usersDataBase) {
-    if (email === usersDataBase[key].email) {
-      return email;
+const checkEmail = (email) => {
+  if (users.email === email) {
+    return true;
+  }
+  return false;
+};
+
+//find user email in database
+
+const checkEmailinDatabase = (email, password) => {
+  // Email doesn't exist
+  if (users.email !== email) {
+    res.status(403).send("Invalid request. Email not found.");
+  }
+
+  // Password doesn't match
+  if (users.password !== password) {
+    res.status(403).send("Invalid request. Wrong password.");
+  }
+
+
+};
+
+const findUserinDatabase = (email) => {
+  for (let user in users) {
+    if (users[user].email === email) {
+      return users[user];
     }
   }
-  return {};
+  return null;
 };
