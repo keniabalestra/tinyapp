6f02a3d97bcd47f5d2bc554262c4011d721088bd
Kenia Balestra <kenia.balestra@gmail.com>
2022-09-09 14:21:22 +0000

    Add permission only to owner to edit/delete URL

--- express_server.js
+++ express_server.js
@@ -66,9 +66,6 @@ app.get("/urls", (req, res) => {
     return res.status(401).send("You need to login to access this feature. Click <a href='/login'>here</a> to login");
   }
   const urls = urlsForUser(currentUser.id, urlDatabase);
-  console.log(currentUser.id)
-  console.log(urls);
-  console.log(urlDatabase);
   const templateVars = { urls: urls, user: users[req.cookies["user_id"]] };
   res.render('urls_index', templateVars);
 });
@@ -79,11 +76,19 @@ app.get("/urls/new", (req, res) => {
     res.redirect('/login');
     return;
   }
+  
   const templateVars = { user: users[req.cookies["user_id"]] };
   res.render('urls_new', templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
+  const currentUser = users[req.cookies["user_id"]];
+  if (!currentUser) {
+    return res.status(401).send("You need to login to access this feature.");
+  }
+  if (urlDatabase[req.params.shortURL].userID !== currentUser){
+    return res.status(403).send("You are not authorized to see this page.");
+  }
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies["user_id"]] };
   res.render('urls_show', templateVars);
 });
@@ -115,15 +120,21 @@ app.get("/u/:shortURL", (req, res) => {
 app.post("/urls/:shortURL", (req, res) => {
   const longURL = req.body.longURL;
   const currentUser = users[req.cookies["user_id"]];
-  if (urlDatabase[req.params.shortURL].userID !== currentUser) {
+  const shortURL =req.params.shortURL;
+  if (urlDatabase[shortURL].userID !== currentUser) {
     return res.status(403).send("You are not authorized to see this page. Please Log In");
   }
-  urlDatabase[req.params.shortURL].longURL = req.body.longURL;
+  urlDatabase[shortURL].longURL = req.body.longURL;
   res.redirect("/urls");
 });
 
 //Delete URL
 app.post("/urls/:shortURL/delete", (req, res) => {
+  const currentUser = users[req.cookies["user_id"]];
+  
+  if(!currentUser){
+    return res.status(403).send("You are not authorized to delete this URL.");
+  }
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
@@ -131,9 +142,9 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 //Login
 app.post('/login', function(req, res) {
-  const currentUser = users[req.cookies["user_id"]]
+  const currentUser = users[req.cookies["user_id"]];
   const user = findUserinDatabase(currentUser, urlDatabase);
-  
+
   //Email not found
   if (user === null) {
     return res.status(403).send("Invalid request. Email not found.");
